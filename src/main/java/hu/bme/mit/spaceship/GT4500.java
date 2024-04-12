63d19e9aa82d34c97fc77fd94058497f7ef6d248
Zsombor Szabadkai <szabadkaizsombor@gmail.com>
2024-04-12 08:55:38 +0200

    Fix #1

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -77,21 +77,23 @@ public class GT4500 implements SpaceShip {
         break;
 
       case ALL:
-      boolean primaryFired = false;
-      boolean secondaryFired = false;
-  
-      if (!primaryTorpedoStore.isEmpty()) {
-          primaryFired = primaryTorpedoStore.fire(1);
-      }
-  
-      if (!secondaryTorpedoStore.isEmpty()) {
-          secondaryFired = secondaryTorpedoStore.fire(1);
-      }
-  
-      firingSuccess = primaryFired || secondaryFired;
-  
-      wasPrimaryFiredLast = true;
-      break;
+        boolean primaryFired = false;
+        boolean secondaryFired = false;
+    
+        if (!primaryTorpedoStore.isEmpty()) {
+            primaryFired = primaryTorpedoStore.fire(1);
+            wasPrimaryFiredLast = true;
+        }
+    
+        if (!secondaryTorpedoStore.isEmpty()) {
+            secondaryFired = secondaryTorpedoStore.fire(1);
+            wasPrimaryFiredLast = false;
+        }
+    
+        firingSuccess = primaryFired && secondaryFired;
+    
+
+        break;
     }
 
     return firingSuccess;
