5b5c04d3b90c3ab787fce617a09e44f7e00dd835
Zsombor Szabadkai <szabadkaizsombor@gmail.com>
2024-04-26 08:56:27 +0200

    new Rand entity removal

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -38,7 +38,7 @@ public class TorpedoStore {
     boolean success = false;
 
     // simulate random overheating of the launcher bay which prevents firing
-    Random generator = new Random();
+    
     double r = generator.nextDouble();
 
     if (r >= FAILURE_RATE) {
